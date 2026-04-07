46e8053cb4d2b4084cf93f5d96a8cdf02ff874a4
K To Mis - 1976 <ktomis2004@gmail.com>
2026-04-07 21:45:50 +0700

    Implement login controller and service with authentication routing

---
+++ src/controller/auth/login.controller.js
@@ -0,0 +1,50 @@
+/**
+ * @copyright 2026 MK-TS-04
+ * @license Apache-2.0
+ */
+
+/**
+ * Custom Moduels
+ */
+import logger from '../../lib/winston.js';
+import config from '../../config/env.config.js';
+
+/**
+ * Service
+ */
+import loginService from '../../service/auth/login.service.js';
+
+const login = async (req, res) => {
+  try {
+    const { user, accessToken, refreshToken } = await loginService(req.body);
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: config.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    res.status(201).json({
+      user: {
+        username: user.username,
+        full_name: user.full_name,
+        email: user.email,
+        password: user.password,
+        role: user.role,
+      },
+      accessToken,
+    });
+
+    logger.info('User login successfully', user);
+  } catch (error) {
+    res.status(500).json({
+      code: 'ServerError',
+      message: 'Internal Server Error',
+    });
+
+    logger.error('Internal Server Error', error);
+  }
+};
+
+export default login;
